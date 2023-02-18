75f6258004e1c5699427da2ff235d4ec4dfc663a
Arthur Pastel <arthur.pastel@gmail.com>
2023-02-18 10:20:55 +0100

    feat: add cache to fibo

--- src/fibonacci.py
+++ src/fibonacci.py
@@ -1,5 +1,10 @@
 def fibonacci(n: int) -> int:
-    if n in [0, 1]:
-        return n
-    return fibonacci(n - 1) + fibonacci(n - 2)
+    cache = {0: 0, 1: 1}
 
+    def inner(n) -> int:
+        if n in cache:
+            return cache[n]
+        cache[n] = inner(n - 1) + inner(n - 2)
+        return cache[n]
+
+    return inner(n)
